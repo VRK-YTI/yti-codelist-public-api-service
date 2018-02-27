992877bde0fb5292ce8c8197e62f05715dd9df57
Antti Tohmo <antti.tohmo@gofore.com>
2018-02-27 22:15:31 +0200

    Adding Validated annotation due to warnings.

--- src/main/java/fi/vm/yti/codelist/api/configuration/PublicApiServiceProperties.java
+++ src/main/java/fi/vm/yti/codelist/api/configuration/PublicApiServiceProperties.java
@@ -4,9 +4,11 @@ import javax.validation.constraints.NotNull;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 @ConfigurationProperties("application")
 @Component
+@Validated
 public class PublicApiServiceProperties {
 
     @NotNull
