35d156f4cedaff7518706165438aff3de99ca757
Antti Tohmo <antti.tohmo@gofore.com>
2018-04-24 17:39:06 +0300

    Moving filters to new package.

---
+++ src/main/java/fi/vm/yti/codelist/api/filter/CacheFilter.java
@@ -0,0 +1,18 @@
+package fi.vm.yti.codelist.api.filter;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class CacheFilter implements ContainerResponseFilter {
+
+    @Override
+    public void filter(final ContainerRequestContext request,
+                       final ContainerResponseContext response) throws IOException {
+        response.getHeaders().add("Cache-Control", "no-cache");
+    }
+}
