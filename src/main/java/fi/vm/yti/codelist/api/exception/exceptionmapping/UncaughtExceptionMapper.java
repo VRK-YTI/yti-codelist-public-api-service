c48927249d242bbb8c152cecc027191004a4ef5d
Tuukka Laitinen <work.ictlaitinen@gmail.com>
2018-06-12 12:25:09 +0300

    Refactoring - adding error logging to ExceptionMappers.

--- src/main/java/fi/vm/yti/codelist/api/exception/exceptionmapping/UncaughtExceptionMapper.java
+++ src/main/java/fi/vm/yti/codelist/api/exception/exceptionmapping/UncaughtExceptionMapper.java
@@ -4,6 +4,8 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 
 import fi.vm.yti.codelist.api.api.ResponseWrapper;
@@ -13,8 +15,11 @@ import fi.vm.yti.codelist.common.dto.Meta;
 @Provider
 public class UncaughtExceptionMapper implements ExceptionMapper<Exception> {
 
+    private static final Logger LOG = LoggerFactory.getLogger(UncaughtExceptionMapper.class);
+
     @Override
     public Response toResponse(final Exception ex) {
+        LOG.error("UnCaughtException occurred: ", ex);
         final ResponseWrapper wrapper = new ResponseWrapper();
         final Meta meta = new Meta();
         meta.setMessage(ErrorConstants.ERR_MSG_USER_500);
--- src/main/java/fi/vm/yti/codelist/api/exception/exceptionmapping/YtiCodeListExceptionMapper.java
+++ src/main/java/fi/vm/yti/codelist/api/exception/exceptionmapping/YtiCodeListExceptionMapper.java
@@ -5,12 +5,17 @@ import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
 import fi.vm.yti.codelist.api.exception.YtiCodeListException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Provider
 public class YtiCodeListExceptionMapper extends BaseExceptionMapper implements ExceptionMapper<YtiCodeListException> {
 
+    private static final Logger LOG = LoggerFactory.getLogger(UncaughtExceptionMapper.class);
+
     @Override
     public Response toResponse(final YtiCodeListException ex) {
+        LOG.error("YtiCodelistException occurred: ", ex);
         return getResponse(ex);
     }
 }
