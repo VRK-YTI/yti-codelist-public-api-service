fade118ffff64569946666afd339fc557cd72ebc
Antti Tohmo <antti.tohmo@gofore.com>
2018-06-26 12:57:46 +0300

    YTI-956: Preflabel support added to Extension.

--- src/main/java/fi/vm/yti/codelist/api/export/ExtensionExporter.java
+++ src/main/java/fi/vm/yti/codelist/api/export/ExtensionExporter.java
@@ -1,5 +1,7 @@
 package fi.vm.yti.codelist.api.export;
 
+import java.util.LinkedHashSet;
+import java.util.Map;
 import java.util.Set;
 
 import org.apache.poi.ss.usermodel.Row;
@@ -14,9 +16,11 @@ import static fi.vm.yti.codelist.common.constants.ApiConstants.*;
 public class ExtensionExporter extends BaseExporter {
 
     public String createCsv(final Set<ExtensionDTO> extensions) {
+        final Set<String> prefLabelLanguages = resolveExtensionPrefLabelLanguages(extensions);
         final String csvSeparator = ",";
         final StringBuilder csv = new StringBuilder();
         appendValue(csv, csvSeparator, CONTENT_HEADER_EXTENSIONVALUE);
+        prefLabelLanguages.forEach(language -> appendValue(csv, csvSeparator, CONTENT_HEADER_PREFLABEL_PREFIX + language.toUpperCase()));
         appendValue(csv, csvSeparator, CONTENT_HEADER_ID);
         appendValue(csv, csvSeparator, CONTENT_HEADER_CODE);
         appendValue(csv, csvSeparator, CONTENT_HEADER_RELATION);
@@ -25,6 +29,7 @@ public class ExtensionExporter extends BaseExporter {
         appendValue(csv, csvSeparator, CONTENT_HEADER_ORDER, true);
         for (final ExtensionDTO extension : extensions) {
             appendValue(csv, csvSeparator, extension.getExtensionValue());
+            prefLabelLanguages.forEach(language -> appendValue(csv, csvSeparator, extension.getPrefLabel().get(language)));
             appendValue(csv, csvSeparator, extension.getId().toString());
             appendValue(csv, csvSeparator, extension.getCode() != null ? extension.getCode().getCodeValue() : "");
             appendValue(csv, csvSeparator, extension.getExtension() != null ? extension.getExtension().getCode().getCodeValue() : "");
@@ -45,11 +50,15 @@ public class ExtensionExporter extends BaseExporter {
     public void addExtensionsSheet(final Workbook workbook,
                                    final String sheetName,
                                    final Set<ExtensionDTO> extensions) {
+        final Set<String> prefLabelLanguages = resolveExtensionPrefLabelLanguages(extensions);
         final Sheet sheet = workbook.createSheet(sheetName);
         final Row rowhead = sheet.createRow((short) 0);
         int j = 0;
         rowhead.createCell(j++).setCellValue(CONTENT_HEADER_ID);
         rowhead.createCell(j++).setCellValue(CONTENT_HEADER_EXTENSIONVALUE);
+        for (final String language : prefLabelLanguages) {
+            rowhead.createCell(j++).setCellValue(CONTENT_HEADER_PREFLABEL_PREFIX + language.toUpperCase());
+        }
         rowhead.createCell(j++).setCellValue(CONTENT_HEADER_CODE);
         rowhead.createCell(j++).setCellValue(CONTENT_HEADER_RELATION);
         rowhead.createCell(j++).setCellValue(CONTENT_HEADER_CREATED);
@@ -61,6 +70,9 @@ public class ExtensionExporter extends BaseExporter {
             int k = 0;
             row.createCell(k++).setCellValue(checkEmptyValue(extension.getId().toString()));
             row.createCell(k++).setCellValue(checkEmptyValue(extension.getExtensionValue()));
+            for (final String language : prefLabelLanguages) {
+                row.createCell(k++).setCellValue(extension.getPrefLabel().get(language));
+            }
             if (extension.getCode() != null) {
                 row.createCell(k++).setCellValue(checkEmptyValue(extension.getCode().getUri()));
             } else {
@@ -75,5 +87,14 @@ public class ExtensionExporter extends BaseExporter {
             row.createCell(k++).setCellValue(extension.getModified() != null ? formatDateWithSeconds(extension.getModified()) : "");
             row.createCell(k).setCellValue(checkEmptyValue(extension.getOrder() != null ? extension.getOrder().toString() : ""));
         }
+    }
+
+    private Set<String> resolveExtensionPrefLabelLanguages(final Set<ExtensionDTO> extensions) {
+        final Set<String> languages = new LinkedHashSet<>();
+        for (final ExtensionDTO extension : extensions) {
+            final Map<String, String> prefLabel = extension.getPrefLabel();
+            languages.addAll(prefLabel.keySet());
+        }
+        return languages;
     }
 }
