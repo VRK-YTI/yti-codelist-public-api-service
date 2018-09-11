459f3f64e083708bbe9370b119b69cc40e4a8116
Antti Tohmo <antti.tohmo@gofore.com>
2018-09-11 08:59:51 +0300

    Bringing back LinkedHashSet typing as impl was not working properly with generic interface Set typing.

--- src/main/java/fi/vm/yti/codelist/api/resource/CodeRegistryResource.java
+++ src/main/java/fi/vm/yti/codelist/api/resource/CodeRegistryResource.java
@@ -528,11 +528,11 @@ public class CodeRegistryResource extends AbstractBaseResource {
                                           @ApiParam(value = "Filter string (csl) for expanding specific child resources.") @QueryParam("expand") final String expand) {
         ObjectWriterInjector.set(new AbstractBaseResource.FilterModifier(createSimpleFilterProvider(FILTER_NAME_CODESCHEME, expand)));
         final CodeSchemeDTO codeScheme = domain.getCodeScheme(codeRegistryCodeValue, codeSchemeCodeValue);
-        Set<CodeSchemeListItem> allVersions = new LinkedHashSet<>();
+        LinkedHashSet<CodeSchemeListItem> allVersions = new LinkedHashSet<>();
         if (codeScheme != null) {
             allVersions = codeScheme.getAllVersions();
         }
-        Set<CodeSchemeDTO> result = new LinkedHashSet<>();
+        LinkedHashSet<CodeSchemeDTO> result = new LinkedHashSet<>();
         final Meta meta = new Meta(200, null, null, null);
 
         if (codeScheme == null) {
@@ -566,11 +566,11 @@ public class CodeRegistryResource extends AbstractBaseResource {
                                           @ApiParam(value = "Filter string (csl) for expanding specific child resources.") @QueryParam("expand") final String expand) {
         ObjectWriterInjector.set(new AbstractBaseResource.FilterModifier(createSimpleFilterProvider(FILTER_NAME_CODESCHEME, expand)));
         final CodeSchemeDTO codeScheme = domain.getCodeScheme(codeRegistryCodeValue, codeSchemeCodeValue);
-        Set<CodeSchemeListItem> variants = new LinkedHashSet<>();
+        LinkedHashSet<CodeSchemeListItem> variants = new LinkedHashSet<>();
         if (codeScheme != null) {
             variants = codeScheme.getVariantsOfThisCodeScheme();
         }
-        Set<CodeSchemeDTO> result = new LinkedHashSet<>();
+        LinkedHashSet<CodeSchemeDTO> result = new LinkedHashSet<>();
         final Meta meta = new Meta(200, null, null, null);
 
         if (codeScheme == null) {
@@ -604,11 +604,11 @@ public class CodeRegistryResource extends AbstractBaseResource {
                                           @ApiParam(value = "Filter string (csl) for expanding specific child resources.") @QueryParam("expand") final String expand) {
         ObjectWriterInjector.set(new AbstractBaseResource.FilterModifier(createSimpleFilterProvider(FILTER_NAME_CODESCHEME, expand)));
         final CodeSchemeDTO codeScheme = domain.getCodeScheme(codeRegistryCodeValue, codeSchemeCodeValue);
-        Set<CodeSchemeListItem> variantMothers = new LinkedHashSet<>();
+        LinkedHashSet<CodeSchemeListItem> variantMothers = new LinkedHashSet<>();
         if (codeScheme != null) {
             variantMothers = codeScheme.getVariantMothersOfThisCodeScheme();
         }
-        Set<CodeSchemeDTO> result = new LinkedHashSet<>();
+        LinkedHashSet<CodeSchemeDTO> result = new LinkedHashSet<>();
         final Meta meta = new Meta(200, null, null, null);
 
         if (codeScheme == null) {
