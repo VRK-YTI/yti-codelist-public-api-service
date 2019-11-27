8e9a50a6678f7e42b011d30e2b4ec143a07fe084
Antti Tohmo <antti.tohmo@gofore.com>
2019-11-27 15:45:52 +0200

    Removing nasty logging from our own exceptions.

--- src/main/java/fi/vm/yti/codelist/api/exception/exceptionmapping/YtiCodeListExceptionMapper.java
+++ src/main/java/fi/vm/yti/codelist/api/exception/exceptionmapping/YtiCodeListExceptionMapper.java
@@ -4,19 +4,13 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import fi.vm.yti.codelist.api.exception.YtiCodeListException;
 
 @Provider
 public class YtiCodeListExceptionMapper implements BaseExceptionMapper, ExceptionMapper<YtiCodeListException> {
 
-    private static final Logger LOG = LoggerFactory.getLogger(UncaughtExceptionMapper.class);
-
     @Override
     public Response toResponse(final YtiCodeListException ex) {
-        LOG.error("YtiCodelistException occurred: ", ex);
         return getResponse(ex);
     }
 }
