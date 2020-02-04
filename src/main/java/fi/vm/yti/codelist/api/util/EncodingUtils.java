6d67c82b8d8158113472fe11deb4c84a31059ebe
Antti Tohmo <antti.tohmo@gofore.com>
2020-02-04 12:49:34 +0200

    YTI-1287: Minor refactoring to use constants.

--- src/main/java/fi/vm/yti/codelist/api/util/EncodingUtils.java
+++ src/main/java/fi/vm/yti/codelist/api/util/EncodingUtils.java
@@ -13,28 +13,36 @@ import static fi.vm.yti.codelist.api.exception.ErrorConstants.ERR_MSG_USER_ERROR
 
 public interface EncodingUtils {
 
+    String UTF_8_ENCODING = "UTF-8";
+    String DOT_VALUE_ENCODED = "U+002E";
+    String DOT_VALUE_ENCODED_VARIANT = "U%2B002E";
+    String DOT_VALUE_DECODED = ".";
+    String DOT_DOT_VALUE_DECODED = "..";
+    String DOT_DOT_VALUE_ENCODED = "U+002EU+002E";
+    String DOT_DOT_VALUE_ENCODED_VARIANT = "U%2B002EU%2B002E";
+
     static String urlDecodeString(final String string) {
         try {
-            return URLDecoder.decode(string, "UTF-8");
+            return URLDecoder.decode(string, UTF_8_ENCODING);
         } catch (final UnsupportedEncodingException e) {
             throw new YtiCodeListException(new ErrorModel(HttpStatus.NOT_ACCEPTABLE.value(), ERR_MSG_USER_406));
         }
     }
 
     static String decodeDotCodeValues(final String codeValue) {
-        if (codeValue.equalsIgnoreCase("U+002E")) {
-            return ".";
-        } else if (codeValue.equalsIgnoreCase("U+002EU+002E")) {
-            return "..";
+        if (codeValue.equalsIgnoreCase(DOT_VALUE_ENCODED)) {
+            return DOT_VALUE_DECODED;
+        } else if (codeValue.equalsIgnoreCase(DOT_DOT_VALUE_ENCODED)) {
+            return DOT_DOT_VALUE_DECODED;
         }
         return codeValue;
     }
 
     static String urlDecodeCodeValue(final String string) {
-        if (string.equalsIgnoreCase("U+002E") || string.equalsIgnoreCase("U%2B002E")) {
-            return ".";
-        } else if (string.equalsIgnoreCase("U+002EU+002E") || string.equalsIgnoreCase("U%2B002EU%2B002E")) {
-            return "..";
+        if (string.equalsIgnoreCase(DOT_VALUE_ENCODED) || string.equalsIgnoreCase(DOT_VALUE_ENCODED_VARIANT)) {
+            return DOT_VALUE_DECODED;
+        } else if (string.equalsIgnoreCase(DOT_DOT_VALUE_ENCODED) || string.equalsIgnoreCase(DOT_DOT_VALUE_ENCODED_VARIANT)) {
+            return DOT_DOT_VALUE_DECODED;
         }
         return urlDecodeString(string);
     }
@@ -43,17 +51,17 @@ public interface EncodingUtils {
         try {
             final String codeValueToBeEncoded;
             switch (codeValue) {
-                case ".":
-                    codeValueToBeEncoded = "U+002E";
+                case DOT_VALUE_DECODED:
+                    codeValueToBeEncoded = DOT_VALUE_ENCODED;
                     break;
-                case "..":
-                    codeValueToBeEncoded = "U+002EU+002E";
+                case DOT_DOT_VALUE_DECODED:
+                    codeValueToBeEncoded = DOT_DOT_VALUE_ENCODED;
                     break;
                 default:
                     codeValueToBeEncoded = codeValue;
                     break;
             }
-            return URLEncoder.encode(codeValueToBeEncoded, "UTF-8");
+            return URLEncoder.encode(codeValueToBeEncoded, UTF_8_ENCODING);
         } catch (final UnsupportedEncodingException e) {
             throw new YtiCodeListException(new ErrorModel(HttpStatus.NOT_ACCEPTABLE.value(), ERR_MSG_USER_ERROR_ENCODING_STRING));
         }
