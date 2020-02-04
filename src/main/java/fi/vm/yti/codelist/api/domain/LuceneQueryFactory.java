187c875b016026cd26ce856d72f47f4d89d87aa1
Antti Tohmo <antti.tohmo@gofore.com>
2020-02-04 12:42:22 +0200

    YTI-1287: Renaming static regex constants to uppercase syntax.

--- src/main/java/fi/vm/yti/codelist/api/domain/LuceneQueryFactory.java
+++ src/main/java/fi/vm/yti/codelist/api/domain/LuceneQueryFactory.java
@@ -20,16 +20,17 @@ import org.springframework.stereotype.Service;
 @Service
 public class LuceneQueryFactory {
 
-    private static final String plainQueryPatternString = "^(?:(?!(?:\\s++|^)(?:AND|OR|TO)(?:\\s|$))(?:\\w++|\\s++|(?<=\\w)-++))+$";
-    private static final String asteriskQueryPatternString = "^(?:(?!(?:\\s++|^)(?:AND|OR|TO)(?:\\s|$))(?:\\w++|\\s++|(?<=[\\w*])-++|(?<!\\*)\\*(?=[\\w-])|(?<=[\\w-])\\*(?!\\*)))+$";
+    private static final String REGEX_PLAIN_QUERY_PATTERN_STRING = "^(?:(?!(?:\\s++|^)(?:AND|OR|TO)(?:\\s|$))(?:\\w++|\\s++|(?<=\\w)-++))+$";
+    private static final String REGEX_ASTERISK_QUERY_PATTERN_STRING = "^(?:(?!(?:\\s++|^)(?:AND|OR|TO)(?:\\s|$))(?:\\w++|\\s++|(?<=[\\w*])-++|(?<!\\*)\\*(?=[\\w-])|(?<=[\\w-])\\*(?!\\*)))+$";
     private static final Logger LOG = LoggerFactory.getLogger(LuceneQueryFactory.class);
-    private final Pattern plainQueryPattern = Pattern.compile(plainQueryPatternString, Pattern.UNICODE_CHARACTER_CLASS);
+
+    private final Pattern plainQueryPattern = Pattern.compile(REGEX_PLAIN_QUERY_PATTERN_STRING, Pattern.UNICODE_CHARACTER_CLASS);
     private final Pattern plainSplitter = Pattern.compile("\\s+", Pattern.UNICODE_CHARACTER_CLASS);
     private final Pattern givenQueryPattern;
 
     @Autowired
     public LuceneQueryFactory() {
-        givenQueryPattern = Pattern.compile(asteriskQueryPatternString, Pattern.UNICODE_CHARACTER_CLASS);
+        givenQueryPattern = Pattern.compile(REGEX_ASTERISK_QUERY_PATTERN_STRING, Pattern.UNICODE_CHARACTER_CLASS);
     }
 
     QueryStringQueryBuilder buildPrefixSuffixQuery(final String searchTerm) {
